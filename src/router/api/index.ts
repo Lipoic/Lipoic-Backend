f1d2590ead4414d4edc0979ae3d41cbc8cc8f01f
菘菘 <rrt467778@gmail.com>
2022-09-20 19:36:30 +0800

    feat: basic router (#7)
    * feat: basic router
    
    * test: delete test comment
    
    * chore: update `.eslintrc.js`
    
    * feat: api response
    
    * feat: 404 not found
    
    * test: not found page
    
    * fix: test

--- src/router/api/index.ts
+++ src/router/api/index.ts
@@ -1,11 +1,15 @@
-import { Router, Request, Response } from 'express';
+import { Code } from '@/router/code';
+import { APIResponse } from '@/router/resopnse';
+import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.json({
-    message: 'Hello, World!',
-  });
+router.get('/', (_, res) => {
+  new APIResponse(Code.ok, { message: 'Hello, World!' }).send(res);
+});
+
+router.use((_, res) => {
+  new APIResponse(Code.not_found).send(res);
 });
 
 export default router;
---
+++ test/router/router.test.ts
@@ -0,0 +1,41 @@
+import { createServer } from '@/util/server';
+import request from 'supertest';
+import { Express } from 'express-serve-static-core';
+import { test, expect, beforeAll } from 'vitest';
+import { init } from '@/util/init';
+
+let server: Express;
+
+beforeAll(() => {
+  init();
+  server = createServer();
+});
+
+test('Hello world', async () => {
+  const response = await request(server).get(`/`);
+
+  expect(response.headers['content-type']).toBe(
+    'application/json; charset=utf-8'
+  );
+  expect(response.status).toBe(200);
+  expect(response.body).toMatchObject({
+    code: 200,
+    message: 'OK',
+    data: {
+      message: 'Hello, World!',
+    },
+  });
+});
+
+test('Not found page', async () => {
+  const response = await request(server).get(`/test`);
+
+  expect(response.headers['content-type']).toBe(
+    'application/json; charset=utf-8'
+  );
+  expect(response.status).toBe(404);
+  expect(response.body).toMatchObject({
+    code: 404,
+    message: 'Resource not found.',
+  });
+});
