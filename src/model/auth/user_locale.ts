263ae337d84192e87b464c544ce0c3d23c4a8115
菘菘 <rrt467778@gmail.com>
2022-10-10 18:39:46 +0800

    docs: user locale

--- src/model/auth/user_locale.ts
+++ src/model/auth/user_locale.ts
@@ -3,7 +3,18 @@
  * @see https://en.wikipedia.org/wiki/IETF_language_tag
  */
 export enum UserLocale {
+  /**
+   * English (United States)
+   */
   AmericanEnglish = 'en-US',
+
+  /**
+   * Traditional Chinese (Taiwan)
+   */
   TraditionalChinese = 'zh-TW',
+
+  /**
+   * Simplified Chinese (China)
+   */
   SimplifiedChinese = 'zh-CN',
 }
