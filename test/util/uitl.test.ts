b17d78b1a33de7d3a6886446d98579c39bf3f496
菘菘 <rrt467778@gmail.com>
2022-12-29 22:01:52 +0800

    test: util#getCharactersLength

--- test/util/uitl.test.ts
+++ test/util/uitl.test.ts
@@ -1,4 +1,4 @@
-import { getEnumValues } from '@/util/util';
+import { getCharactersLength, getEnumValues } from '@/util/util';
 import { describe, expect, test } from 'vitest';
 
 describe('Get the values of an enum as an array of strings', () => {
@@ -36,5 +36,38 @@ describe('Get the values of an enum as an array of strings', () => {
     const result = getEnumValues(Foo);
 
     expect(result).toEqual(['A', 'B', 'C']);
+  });
+});
+
+describe('Get characters length of the string', () => {
+  test('Hello world', () => {
+    const result = getCharactersLength('Hello, world!');
+
+    expect(result).toBe(13);
+  });
+
+  test('Emoji', () => {
+    const result = getCharactersLength('👋🌍');
+    expect(result).toBe(2);
+  });
+
+  test('Emoji with skin tone', () => {
+    const result = getCharactersLength('👋🏻🌍');
+    expect(result).toBe(3);
+  });
+
+  test('Chinese', () => {
+    const result = getCharactersLength('你好，世界！');
+    expect(result).toBe(6);
+  });
+
+  test('Japanese', () => {
+    const result = getCharactersLength('こんにちは世界！');
+    expect(result).toBe(8);
+  });
+
+  test('Special characters', () => {
+    const result = getCharactersLength('!@#$%^&*()_+');
+    expect(result).toBe(12);
   });
 });
